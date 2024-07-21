0d40f85c87918430b09bf850ca6746799ae1f2d4
manish vyas <manishvyas393@gmail.com>
2024-07-21 07:14:23 +0530

    headers and heading

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const StartScreen = ({ questions }) => {
   return (
     <div className="start">
-      <h2 className="text-white text-6xl font-bold capitalize">Welcome to the react quiz</h2>
+      <h2 className="text-white text-6xl font-bold capitalize">Welcome to the React quiz</h2>
       <h3 className="text-3xl text-white mt-8"> {questions} Questions to test your react knowlegde</h3>
       <button className="mt-4 text-white bg-gray-400 px-8 py-2 rounded-2xl">Let's start</button>
     </div>
