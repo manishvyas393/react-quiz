594a511fcbc826e7cb79c9e7154c145ed89f1413
manish vyas <manishvyas393@gmail.com>
2024-07-21 17:39:23 +0530

    loader

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "../logo.svg";
 const Header = () => {
   return (
     <header className="flex justify-center items-center my-5">
-      <img src={Logo} alt="" className="md:h-44 h-16" />
+      <img src={Logo} alt="" className="md:h-44 h-16 md:-mr-10" />
 
       <h1 className="md:text-8xl text-5xl text-white"> The React Quiz</h1>
     </header>
